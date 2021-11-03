8b38da2ecd0e7ca22b279978418f594079053e03
zdm <zdm@softvisio.net>
2021-11-03 14:33:25 +0200

    style: sql

--- softvisio--0.0.0--1.0.0.sql
+++ softvisio--0.0.0--1.0.0.sql
@@ -1,71 +1,71 @@
 \echo 'You need to use the following commands:'
-\echo 'CREATE EXTENSION IF NOT EXISTS "softvisio" CASCADE;'
-\echo 'ALTER EXTENSION "softvisio" UPDATE;'
+\echo 'CREATE EXTENSION IF NOT EXISTS softvisio CASCADE;'
+\echo 'ALTER EXTENSION softvisio UPDATE;'
 \echo \quit
 
-CREATE OR REPLACE PROCEDURE create_database(_name TEXT, _collate TEXT DEFAULT 'ru_UA.UTF-8') AS $$
+CREATE OR REPLACE PROCEDURE create_database ( _name text, _collate text DEFAULT 'ru_UA.UTF-8' ) AS $$
 DECLARE
-    _password TEXT;
-    _conn TEXT;
+    _password text;
+    _conn text;
 BEGIN
     _conn := 'dbname=' || _name;
 
     -- check database exists
-    IF EXISTS (SELECT 1 FROM "pg_database" WHERE "datname" = _name) THEN
+    IF EXISTS ( SELECT 1 FROM pg_database WHERE datname = _name ) THEN
         RAISE EXCEPTION 'Database already exists';
     END IF;
 
     -- create database
-    PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE ' || quote_ident(_name) || ' ENCODING ''UTF8'' LC_COLLATE ' || quote_literal(_collate) || ' LC_CTYPE ' || quote_literal(_collate) || ' TEMPLATE template0', FALSE);
+    PERFORM dblink_exec( 'dbname=' || current_database(), 'CREATE DATABASE ' || quote_ident( _name ) || ' ENCODING ''UTF8'' LC_COLLATE ' || quote_literal( _collate ) || ' LC_CTYPE ' || quote_literal( _collate ) || ' TEMPLATE template0', FALSE);
 
     -- if user is not exists
-    IF NOT EXISTS (SELECT 1 FROM "pg_roles" WHERE "rolname" = _name) THEN
+    IF NOT EXISTS ( SELECT 1 FROM pg_roles WHERE rolname = _name ) THEN
         -- generate password
-        _password := (SELECT translate(encode(gen_random_bytes(16), 'base64'), '+/=', '-_') AS "password");
+        _password := ( SELECT translate( encode( gen_random_bytes( 16 ), 'base64' ), '+/=', '-_' ) AS password );
 
         RAISE NOTICE 'Password %', _password;
 
         -- create user
-        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE USER ' || quote_ident(_name) || ' WITH ENCRYPTED PASSWORD ' || quote_literal(_password), FALSE);
+        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE USER ' || quote_ident( _name ) || ' WITH ENCRYPTED PASSWORD ' || quote_literal( _password ), FALSE );
     ELSE
         RAISE NOTICE 'User already exists, you can set password manually';
     END IF;
 
     -- grant user permissions to create schemas
-    PERFORM dblink_exec('dbname=' || current_database(), 'GRANT ALL PRIVILEGES ON DATABASE ' || quote_ident(_name) || ' TO ' || quote_ident(_name), FALSE);
+    PERFORM dblink_exec( 'dbname=' || current_database(), 'GRANT ALL PRIVILEGES ON DATABASE ' || quote_ident( _name ) || ' TO ' || quote_ident( _name ), FALSE );
 
     -- create schema
-    PERFORM dblink_exec(_conn, 'CREATE SCHEMA AUTHORIZATION ' || quote_ident(_name), FALSE);
+    PERFORM dblink_exec( _conn, 'CREATE SCHEMA AUTHORIZATION ' || quote_ident( _name ), FALSE );
 
     -- create extensions in "public" schema
-    PERFORM dblink_exec(_conn, 'CREATE EXTENSION "pgcrypto" CASCADE', FALSE);
-    PERFORM dblink_exec(_conn, 'CREATE EXTENSION "timescaledb" CASCADE', FALSE);
-    -- PERFORM dblink_exec(_conn, 'CREATE EXTENSION "pg_hashids" CASCADE', FALSE);
+    PERFORM dblink_exec(_conn, 'CREATE EXTENSION pgcrypto CASCADE', FALSE);
+    PERFORM dblink_exec(_conn, 'CREATE EXTENSION timescaledb CASCADE', FALSE);
+    -- PERFORM dblink_exec(_conn, 'CREATE EXTENSION pg_hashids CASCADE', FALSE);
 END;
-$$ LANGUAGE "plpgsql";
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE update_extensions() AS $$
 DECLARE
-    _dbname TEXT;
-    _ext_name TEXT;
+    _dbname text;
+    _ext_name text;
 BEGIN
     FOR _dbname IN
-        SELECT "datname" FROM "pg_database" WHERE "datistemplate" = FALSE
+        SELECT datname FROM pg_database WHERE datistemplate = FALSE
     LOOP
         RAISE NOTICE 'DB name "%"', _dbname;
 
-        PERFORM dblink_connect('dbname=' || _dbname);
+        PERFORM dblink_connect( 'dbname=' || _dbname );
 
         -- test query, required to avoid exceptions when some extension can't be loaded
-        _ext_name := ( SELECT "test" FROM dblink('SELECT 1', FALSE) AS t("test" int2) );
+        _ext_name := ( SELECT test FROM dblink( 'SELECT 1', FALSE ) AS t( test int2 ) );
 
         FOR _ext_name IN
-            SELECT "extname" FROM dblink('SELECT "extname" FROM "pg_extension"') AS t("extname" text)
+            SELECT extname FROM dblink( 'SELECT extname FROM pg_extension' ) AS t( extname text )
         LOOP
-            PERFORM dblink_exec('dbname=' || _dbname, 'ALTER EXTENSION ' || quote_ident(_ext_name) || ' UPDATE', FALSE);
+            PERFORM dblink_exec( 'dbname=' || _dbname, 'ALTER EXTENSION ' || quote_ident( _ext_name ) || ' UPDATE', FALSE );
         END LOOP;
 
         PERFORM dblink_disconnect();
     END LOOP;
 END;
-$$ LANGUAGE "plpgsql";
+$$ LANGUAGE plpgsql;
